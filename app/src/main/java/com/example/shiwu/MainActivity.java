1527cf990119d2a9159a220e13d4f0745b4216fb
yukaida <1204799167@qq.com>
2019-08-25 20:07:32 +0800

    点击导航栏切换Fragment

--- app/src/main/java/com/example/shiwu/MainActivity.java
+++ app/src/main/java/com/example/shiwu/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.shiwu;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.shiwu.Fragment.EmptyFragment;
+import com.example.shiwu.Fragment.HomeFragment;
+import com.example.shiwu.Fragment.KindFragment;
+import com.example.shiwu.Fragment.MesFragment;
+import com.example.shiwu.Fragment.MyFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity  implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -21,8 +29,11 @@ public class MainActivity extends AppCompatActivity  implements BottomNavigation
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        replaceFragment(new HomeFragment());
+        bottomNavigationView.setSelectedItemId(bottomNavigationView.getMenu().getItem(0).getItemId());
     }
-    public void initView(){
+
+    public void initView() {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.tab_two);
@@ -32,17 +43,21 @@ public class MainActivity extends AppCompatActivity  implements BottomNavigation
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int itemId = menuItem.getItemId();
-        switch (itemId){
+        switch (itemId) {
             case R.id.tab_one:
+                replaceFragment(new HomeFragment());
                 menuItem.setChecked(true);
                 break;
             case R.id.tab_two:
+                replaceFragment(new KindFragment());
                 menuItem.setChecked(true);
                 break;
             case R.id.tab_three:
+                replaceFragment(new MesFragment());
                 menuItem.setChecked(true);
                 break;
             case R.id.tab_four:
+                replaceFragment(new MyFragment());
                 menuItem.setChecked(true);
                 break;
         }
@@ -50,6 +65,10 @@ public class MainActivity extends AppCompatActivity  implements BottomNavigation
     }
 
 
-
-
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_empty, fragment);
+        transaction.commit();
+    }
 }
