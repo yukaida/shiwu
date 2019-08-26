e973da4740a527e9eaea727b3228a9ef91bb46f1
yukaida <1204799167@qq.com>
2019-08-26 16:48:44 +0800

    添加注释

--- app/src/main/java/com/example/shiwu/MainActivity.java
+++ app/src/main/java/com/example/shiwu/MainActivity.java
@@ -29,23 +29,23 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        replaceFragment(new HomeFragment());
-        bottomNavigationView.setSelectedItemId(bottomNavigationView.getMenu().getItem(0).getItemId());
+        replaceFragment(new HomeFragment()); //启动时用HomeFragment替换掉空Fragment
     }
 
     public void initView() {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        bottomNavigationView.setSelectedItemId(R.id.tab_two);
-        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);//设置导航栏监听器
+        bottomNavigationView.setSelectedItemId(R.id.tab_one);//设置默认选择的导航栏子项tab_one即首页
+        bottomNavigationView.setItemIconTintList(null);//取消导航栏子项图片的颜色覆盖
     }
 
     @Override
+    //处理导航栏子项的点击事件
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        int itemId = menuItem.getItemId();
+        int itemId = menuItem.getItemId();//获取点击的位置以及对应的id
         switch (itemId) {
             case R.id.tab_one:
-                replaceFragment(new HomeFragment());
+                replaceFragment(new HomeFragment());//id为tab_one则第一项被点击，遂用HomeFragment替换空Fragment
                 menuItem.setChecked(true);
                 break;
             case R.id.tab_two:
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         return false;
     }
 
-
+//替换Fragment的方法
     public void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
