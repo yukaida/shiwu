2dc0f0c47a4498384cfc40ac3cae6195b5be1048
yukaida <1204799167@qq.com>
2019-08-29 18:26:47 +0800

    添加轮播图

--- app/src/main/java/com/example/shiwu/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/shiwu/Fragment/HomeFragment.java
@@ -1,32 +1,90 @@
 package com.example.shiwu.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.shiwu.MainActivity;
 import com.example.shiwu.R;
-import com.example.shiwu.RecyclerView_item.HomeF_item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
+    private ViewPager viewPager;
+    private View view;
+    private List<View> viewPagerlist = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-//        initView();
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        initView();
+        initViewPager_item();
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return viewPagerlist.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+                return view == object;
+            }
+
+            @NonNull
+            @Override
+            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+                container.addView(viewPagerlist.get(position));
+                return viewPagerlist.get(position);
+            }
+
+            @Override
+            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+                container.removeView(viewPagerlist.get(position));
+
+            }
+        });
 
         return view;
     }
 
-    private void initView(){
-        recyclerView = getView().findViewById(R.id.recyclerView_homeF);
+    private void initView() {
+        recyclerView = view.findViewById(R.id.recyclerView_homeF);
+        viewPager = view.findViewById(R.id.viewPager_HomeF);
+    }
+
+    private void initViewPager_item() {
+        {
+            View view = View.inflate(getContext(), R.layout.fragment_home_viewpager_item, null);
+            ImageView imageView = view.findViewById(R.id.imageView_homeF_viewpager);
+            imageView.setBackgroundColor(Color.RED);
+            viewPagerlist.add(view);
+        }
+        {
+            View view = View.inflate(getContext(), R.layout.fragment_home_viewpager_item, null);
+            ImageView imageView = view.findViewById(R.id.imageView_homeF_viewpager);
+            imageView.setBackgroundColor(Color.GREEN);
+            viewPagerlist.add(view);
+        }
+        {
+            View view = View.inflate(getContext(), R.layout.fragment_home_viewpager_item, null);
+            ImageView imageView = view.findViewById(R.id.imageView_homeF_viewpager);
+            imageView.setBackgroundColor(Color.BLUE);
+            viewPagerlist.add(view);
+        }
     }
 
 
--- app/src/main/java/com/example/shiwu/RecyclerView_item/HomeF_item.java
+++ app/src/main/java/com/example/shiwu/RecyclerView_item/HomeF_item.java
@@ -1,6 +1,46 @@
 package com.example.shiwu.RecyclerView_item;
 
+import java.util.Objects;
+
 public class HomeF_item {
     int name;
     int data;
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HomeF_item that = (HomeF_item) o;
+        return name == that.name &&
+                data == that.data;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, data);
+    }
+
+    @Override
+    public String toString() {
+        return "HomeF_item{" +
+                "name=" + name +
+                ", data=" + data +
+                '}';
+    }
 }
